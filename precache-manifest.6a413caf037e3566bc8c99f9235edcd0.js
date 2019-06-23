89ff91568881bb2820ae0ccccb7707e6c8fcb9d6
IslaMurtazaev <islam.muratazaev@gmail.com>
2019-06-23 20:42:03 +0600

    Updates

---
+++ precache-manifest.6a413caf037e3566bc8c99f9235edcd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c6f65b5467bae2d387c",
+    "url": "/IslaMurtazaev/portfolio/static/js/runtime~main.3c6f65b5.js"
+  },
+  {
+    "revision": "9417b369f7342aef0f3e",
+    "url": "/IslaMurtazaev/portfolio/static/js/main.9417b369.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/IslaMurtazaev/portfolio/static/js/2.9eee67cf.chunk.js"
+  },
+  {
+    "revision": "9417b369f7342aef0f3e",
+    "url": "/IslaMurtazaev/portfolio/static/css/main.fdf6462c.chunk.css"
+  },
+  {
+    "revision": "0344dc2ba85ac0178c467104242e03da",
+    "url": "/IslaMurtazaev/portfolio/index.html"
+  }
+];
